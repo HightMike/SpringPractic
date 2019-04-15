2eb61403a7a7ccb100ade5bccfd1282b674c6657
HightMike <baobab-borisov@mail.ru>
2019-04-15 20:33:29 +0300

    Внедрение зависимостей через xml

--- src/main/java/ru/borisov/springcourse/TestSpring.java
+++ src/main/java/ru/borisov/springcourse/TestSpring.java
@@ -6,9 +6,9 @@ public class TestSpring {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music testBean = context.getBean("musicBean", Music.class); // приводится к этому классу (второй параметр)
-        MusicPlayer musicPlayer = new MusicPlayer(testBean); // IOC внедрили зависимость
-        musicPlayer.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); // приводится к этому классу (второй параметр)
+        //MusicPlayer musicPlayer = new MusicPlayer(testBean); // IOC внедрили зависимость
+        musicPlayer.playMusic(); // внедряются зависимости через xml файл
 
 
         context.close();
